fb7a77ac8b80b34e777fdf212a3e094e276d7201
Zoë Davidson <zoe.a.h.davidson@gmail.com>
2022-03-31 16:33:38 -0400

    images added for resize

--- src/components/StreamDisplay.js
+++ src/components/StreamDisplay.js
@@ -4,6 +4,9 @@ import { useShowScreenshareLarge } from "../context/CreateScreenshareContext";
 import { useShowVideoFeedLarge } from "../context/CreateVideoFeedContext";
 import { useState } from "react";
 
+import selectedImage from "../assets/images/selected-video-feed.png";
+import unselectedImage from "../assets/images/unselected-video-feed.png";
+
 const StreamDisplay = () => {
   const showScreenshareLarge = useShowScreenshareLarge();
   const showVideoFeedLarge = useShowVideoFeedLarge();
@@ -17,8 +20,7 @@ const StreamDisplay = () => {
         <div
           id="video-wrapper"
           className={
-            size
-            // showVideoFeedLarge && showScreenshareLarge ? display : size
+            showVideoFeedLarge && showScreenshareLarge ? display : size
           }
         >
           <video id="large-video" className="large-video" autoPlay></video>
@@ -32,7 +34,6 @@ const StreamDisplay = () => {
               onClick={(e) => {
                 e.preventDefault();
                 setDisplay("display1");
-                console.log(display);
               }}
             >
               Display 1
@@ -41,7 +42,6 @@ const StreamDisplay = () => {
               onClick={(e) => {
                 e.preventDefault();
                 setDisplay("display2");
-                console.log(display);
               }}
             >
               Display 2
@@ -50,7 +50,6 @@ const StreamDisplay = () => {
               onClick={(e) => {
                 e.preventDefault();
                 setDisplay("display3");
-                console.log(display);
               }}
             >
               Display 3
@@ -58,33 +57,32 @@ const StreamDisplay = () => {
           </div>
         ) : (
           <div id="video-feed-buttons">
-            <button
+            <img
+              alt="size1"
+              src={size === "size1" ? selectedImage : unselectedImage}
               onClick={(e) => {
                 e.preventDefault();
                 setSize("size1");
-                console.log(size);
               }}
-            >
-              Size 1
-            </button>
-            <button
+            />
+
+            <img
+              alt="size2"
+              src={size === "size2" ? selectedImage : unselectedImage}
               onClick={(e) => {
                 e.preventDefault();
                 setSize("size2");
-                console.log(size);
               }}
-            >
-              Size 2
-            </button>{" "}
-            <button
+            />
+
+            <img
+              alt="size3"
+              src={size === "size3" ? selectedImage : unselectedImage}
               onClick={(e) => {
                 e.preventDefault();
                 setSize("size3");
-                console.log(size);
               }}
-            >
-              Size 3
-            </button>
+            />
           </div>
         )}
       </div>
